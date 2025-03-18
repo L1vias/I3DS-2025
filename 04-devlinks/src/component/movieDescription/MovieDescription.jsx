a985555f8195642d80bf1c0c9e2a54a0c13d9100
Livia Solla <limasenai12@gmail.com>
2025-03-18 16:32:39 -0300

    MovieCardStyles

--- 04-devlinks/src/component/movieDescription/MovieDescription.jsx
+++ 04-devlinks/src/component/movieDescription/MovieDescription.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import devFlix from "/favicon.png";
+import styles from "./MovieDescription.module.css";
 
 const MovieDescription = (props) => {
   const [MovieDesc, setMovieDesc] = useState([]);
