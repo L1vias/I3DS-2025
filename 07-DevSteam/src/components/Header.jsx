4e2ad01ab32b03814c6cdabb93ba59f0ca9a2195
Livia Solla <limasenai12@gmail.com>
2025-04-22 10:41:56 -0300

    ajustes App.css

--- 07-DevSteam/src/components/Header.jsx
+++ 07-DevSteam/src/components/Header.jsx
@@ -9,11 +9,12 @@ const Header = () => {
           <span className="navbar-brand fw-bold fs-3">DevSteam</span>
         </div>
 
-        <input type="text" 
-        className="w-100 d-none d-md-block border-0 rounded-1 buscar px-4 my-2 "
-        placeholder="Buscar..." />
+        <input
+          type="text"
+          className="w-100 d-none d-md-block border-0 rounded-1 buscar px-4 my-2"
+          placeholder="Buscar..."
+        />
       </div>
-
       <div id="carrinho" role="button">
         <i className="bi bi-cart4 text-light fs-2"></i>
       </div>
